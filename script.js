22c8283d1f5adafcf97ac101241b78bf9f4d5d39
yossi shapira <yosi105009@gmail.com>
2026-02-05 17:55:51 +0200

    end

--- script.js
+++ script.js
@@ -1,28 +1,68 @@
-let score;
+let scores = [0, 0];
+let currentPlayer = 1
+
 startGame();
 
+document.getElementById("dice-1").textContent = "dice 1: ";
+document.getElementById("dice-2").textContent = "dice 2: ";
+const hold = document.querySelector('#hold');
+const rolling = document.querySelector('#rollDice');
+let  score = 0
 function startGame() {
-    // secretNumber = Math.floor(Math.random() * 6) + 1;
-    // console.log(secretNumber);
+    document.getElementById(`scorePlayer${currentPlayer}`).textContent =
+        scores[currentPlayer - 1];
 
-    // score = 0;
-    // document.getElementById("score").textContent = score;
-    // document.getElementById("message").textContent = "";
-    // document.getElementById("message").className = "";
-    // document.getElementById("playAgain").style.display = "none";
+    document.getElementById("message-end").textContent = "";
 }
 
-function diceRolling(){
-    let dice1 = Math.floor(Math.random() * 6) + 1;
-    let dice2 = Math.floor(Math.random() * 6) + 1;
-    console.log(dice1);
-    console.log(dice2);
-    if (dice1 === dice2){
-     return false
-    } 
-    let sumDice = dice1+dice2
-    console.log(sumDice);
-    
-    return sumDice
-}
-diceRolling()
+hold.addEventListener('click', (event) => {
+    if (event.target) {
+
+        scores[currentPlayer - 1] += score;
+
+        document.getElementById(`scorePlayer${currentPlayer}`).textContent =
+            scores[currentPlayer - 1];
+
+        if (scores[currentPlayer - 1] >= 100) {
+            document.getElementById("message-end").textContent =
+                `Player ${currentPlayer} Won!`;
+            return;
+        }
+
+        score = 0;
+
+        if (currentPlayer === 1) {
+            currentPlayer = 2;
+        } else {
+            currentPlayer = 1;
+        }
+    }
+});
+
+rolling.addEventListener('click', (event) => {
+    if (event.target) {
+
+        const dice_1 = document.getElementById("dice-1");
+        const dice_2 = document.getElementById("dice-2");
+
+        let dice1 = Math.floor(Math.random() * 6) + 1;
+        let dice2 = Math.floor(Math.random() * 6) + 1;
+
+        dice_1.textContent = "dice 1: " + dice1;
+        dice_2.textContent = "dice 2: " + dice2;
+
+        if (dice1 === dice2) {
+            score = 0;
+            currentPlayer = currentPlayer === 1 ? 2 : 1;
+            return;
+        }
+
+        let sumDice = dice1 + dice2;
+        score += sumDice;
+    }
+});
+
+
+
+
+
